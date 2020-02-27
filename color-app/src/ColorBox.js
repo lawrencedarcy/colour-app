3f5552f0fbffca268634f1478e72f4a2d6dcd03a
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-02-27 16:38:56 +0000

    add dynamic text colors to improve readability

--- color-app/src/ColorBox.js
+++ color-app/src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 
 class ColorBox extends Component {
   constructor(props) {
@@ -24,23 +25,27 @@ class ColorBox extends Component {
   render() {
     const { name, background, paletteId, id, showLink } = this.props;
     const { copied } = this.state;
+    const isDark = chroma(background).luminance() <= 0.06;
+    const isLight = chroma(background).luminance() >= 0.07;
+
 
     return (
 
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
           <div style={{ background }} className={`copy-overlay ${copied && "show"}`} />
-          <div className={`copy-msg ${copied && "show"}`}> <h1>Copied to clipboard</h1>
-    <p>{background}</p>
+          <div className={`copy-msg ${copied && "show"}`}> 
+          <h1>Copied to clipboard</h1>
+    <p className={`${isLight && "dark-text" }`}>{background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span>{name}</span>
+              <span className={isDark && "light-text"}>{name}</span>
               </div>
-              <button className="copy-button">Copy</button>
+              <button className={`copy-button ${isLight && "dark-text" }`}>Copy</button>
               </div>
               {showLink && (<Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
-              <span className="see-more">More</span>
+              <span className={`see-more ${isLight && "dark-text" } `}>More</span>
               </Link>)}
               
               </div>
